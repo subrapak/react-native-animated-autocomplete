113a1ddf2924d80ed9a5755a5315f055a01d33fe
Arohan <arohans@theodo.co.uk>
2021-01-13 17:53:44 +0000

    initial commit yarn

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import AnimatedAutocomplete from 'react-native-animated-autocomplete';
 
-
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
